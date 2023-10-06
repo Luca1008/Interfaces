89751157c62c357e057770c68c79a3ea533048c5
Luca1008 <111925104+Luca1008@users.noreply.github.com>
2023-10-05 22:05:23 -0300

    Guardar para no ahacer cagaada
    aqgqegqtg

--- SegundaEntrega/utility.js
+++ SegundaEntrega/utility.js
@@ -11,3 +11,28 @@ document.addEventListener("DOMContentLoaded", function() {
         menuPerfilUser.classList.remove("abierto");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleMenuBtn = document.getElementById("toggleMenu");
+    const menuHamburguesa = document.getElementById("menuHamburguesa");
+
+    let isOpen = false; // Variable para rastrear si el menú está abierto o cerrado
+
+    // Función para alternar la visibilidad del menú y rotar la imagen
+    function toggleMenu() {
+        isOpen = !isOpen;
+
+        // Aplicar rotación dependiendo del estado del menú
+        if (isOpen) {
+            toggleMenuBtn.classList.add("rotated");
+        } else {
+            toggleMenuBtn.classList.remove("rotated");
+        }
+
+        // Alternar la clase "mostrado" en el menú
+        menuHamburguesa.classList.toggle("mostrado");
+    }
+
+    // Evento de clic para abrir o cerrar el menú
+    toggleMenuBtn.addEventListener("click", toggleMenu);
+});
