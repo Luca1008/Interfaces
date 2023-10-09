340e28478d123910afb9b4c6aa0a7541cb889542
Luca1008 <111925104+Luca1008@users.noreply.github.com>
2023-10-08 23:14:31 -0300

    asdasdasd
    asdasdadsa

--- SegundaEntrega/utility.js
+++ SegundaEntrega/utility.js
@@ -1,38 +1,65 @@
+// Variables para la primera sección de carrusel
+let slideIndexSection1 = 1;
+showSlides(slideIndexSection1, 'forYou'); // Agregar 'section1' como segundo argumento
 
+// Variables para la segunda sección de carrusel
+let slideIndexSection2 = 1;
+showSlides(slideIndexSection2, 'action'); // Agregar 'section2' como segundo argumento
 
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, section) {
+    if (section === 'forYou') {
+        showSlides(slideIndexSection1 += n, 'forYou');
+    } else if (section === 'action') {
+        showSlides(slideIndexSection2 += n, 'action');
+    }
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, section) {
+    if (section === 'forYou') {
+        showSlides(slideIndexSection1 = n, 'forYou');
+    } else if (section === 'action') {
+        showSlides(slideIndexSection2 = n, 'action');
+    }
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+function showSlides(n, section) {
+    let i;
+    console.log(`mySlides ${section}`);
+    let slides = document.getElementsByClassName(`mySlides ${section}`);
+   let nextt= document.querySelector('.next');
+   let previo=document.querySelector('.prev');
+    let dots = document.getElementsByClassName(`dot ${section}`);
+    console.log(`dot ${section}`)
+    console.log(n)
+
+    if(n===3){
+     nextt.classList.add('hidde');
+     previo.classList.remove('hidde');
+    }
+    if (n!=3){
+  nextt.classList.remove('hidde');
+  previo.classList.remove('hidde');
 }
-
-
-
-
-
-
-
-
+if (n==1){
+  previo.classList.add('hidde');
+}
+    if (n > slides.length) { 
+        n = 1;
+       
+    }
+    if (n < 1) { 
+        n = slides.length;
+       
+    }
+   
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[n-1].style.display = "block";
+    dots[n-1].className += " active";
+    
+   
+}
